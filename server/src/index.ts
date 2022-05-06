11bbe1423bac33359f59fb6a22527f32f3595da1
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-05-06 13:48:03 -0300

    Dia 05

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import { routes } from "./routes";
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json({ limit: "900kb" }));
 app.use(express.json());
 app.use(routes);
 
